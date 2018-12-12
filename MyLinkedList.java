055a8ff07de4852688ea4023847722904ba346fc
Isabella Rocha <irocha00@stuy.edu>
2018-12-12 16:28:15 -0500

    Wrote constructors

---
+++ MyLinkedList.java
@@ -0,0 +1,18 @@
+public class MyLinkedList {
+  private int size;
+  private Node start,end;
+
+  public MyLinkedList() {
+    start = new Node(null, end);
+    end = new Node(start, null);
+  }
+  public int size() {
+
+  }
+  public boolean add(int value) {
+
+  }
+  public String toString() {
+
+  }
+}
---
+++ Node.java
@@ -0,0 +1,9 @@
+public class Node {
+  private int data;
+  private Node next,prev;
+
+  public Node(int data, Node next) {
+    this.data = data;
+    this.next = next;
+  }
+}
